991625022c2d5215ab4fbb82c992a63961679d00
Ahmedkel <ahmedkeloch@gmail.com>
2024-03-12 16:03:45 +0000

    realign the server.js

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const app = express();
 const indexRouter = require('./routes/index');
 
+
+const app = express();
 const port = process.env.PORT || 5000;
 
 app.use('/', indexRouter);
