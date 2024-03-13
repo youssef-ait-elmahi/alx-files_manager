c1d65625758fca1772791eb96c557bec09ba5dd5
Ahmedkel <ahmedkeloch@gmail.com>
2024-03-13 13:30:37 +0000

    revert to old server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
+const app = express();
 const indexRouter = require("./routes/index");
 
-const app = express();
 const port = process.env.PORT || 5000;
 
 app.use("/", indexRouter);
